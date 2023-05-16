64bb29316cc2bd6fc9ae6db96b4033718114ee79
Sinerror <Sinerror.sin@gmail.com>
2023-05-16 10:30:40 +0300

    modified:   server/index.js 	new file:   server/router/index.js     new file:   server/controllers/user_controller.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
-
+const router = require("./router/index")
 const PORT = process.env.PORT || 5000;
 const app = express();
 
---
+++ server/router/index.js
@@ -0,0 +1,10 @@
+const Router = require("express").Router
+const router = Router()
+
+const users = require('../controllers/user_controller')
+router.get("/user", users.GetUser )
+router.post("/adduser")
+router.delete("/deluser")
+router.post("/upduser");
+
+module.exports = router

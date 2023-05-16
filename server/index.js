ba864ea051ccc9e286b67123c0e3728b22d6bb49
Sinerror <Sinerror.sin@gmail.com>
2023-05-16 10:24:15 +0300

    modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -9,13 +9,12 @@ const app = express();
 
 const start = async() => {
     try{
-        app.listen(PORT, () => console.log(`
-        
-        
-        server started on port = ${PORT}`))
+        app.listen(PORT, () => console.log(`server started on port = ${PORT}`))
     }
-    catch(e){
-        console.log(e)
-    }
+    catch(e){console.log(e);}
 }  
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use("/api", router)
 start()

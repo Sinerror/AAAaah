6d744db7e7ace18ac4cdc4abda5a10505878b9ad
Sinerror <Sinerror.sin@gmail.com>
2023-05-16 10:15:54 +0300

    new file:   controllers/user_controller.js modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const cors = require('express');
+const cors = require('cors');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const app = express();
+
 const start = async() => {
     try{
         app.listen(PORT, () => console.log(`

3cc2b459e56c127e593bdf3e35e7c08fa8273970
Sinerror <Sinerror.sin@gmail.com>
2023-05-18 09:20:08 +0300

    modified:   server/index.js 	modified:   server/node_modules/.package-lock.json 	modified:   server/package-lock.json 	modified:   server/package.json

--- server/index.js
+++ server/index.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 
 const cookieParser = require('cookie-parser');
 const express = require('express');
-const cors = require('cors');
-const router = require("./router/index")
-const PORT = process.env.PORT || 5000;
-const app = express();
+const cors = require('express');
 
+const PORT = process.env.PORT;
+const app = express();
 const start = async() => {
     try{
-        app.listen(PORT, () => console.log(`server started on port = ${PORT}`))
+        app.listen(PORT, () => console.log(`
+        
+        
+        server started on port = ${PORT}`))
     }
-    catch(e){console.log(e);}
+    catch(e){
+        console.log(e)
+    }
 }  
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
-app.use("/api", router)
 start()
